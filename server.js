f1aeb038f4d1f6796c1c6fd0dfe1bd8d3248a435
Austin Wong <95609378+AYWong0305@users.noreply.github.com>
2026-05-10 15:11:02 -0700

    Initialize Express server with routes and DB connection

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import gamesRoutes from "./routes/games.js";
+import historyRoutes from "./routes/history.js";
+
+import { connectToDatabase } from "./services/db.js";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an Express application
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+await connectToDatabase();
+
+// Routes
+app.use("/games", gamesRoutes);
+app.use("/history", historyRoutes);
+
+// Home route
+app.get("/", (req, res) => {
+    res.json({
+        message: "FreeToGame API Server Running"
+    });
+});
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
